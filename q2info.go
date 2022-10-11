ef5c047abd4e8224fcfbca53ee0dcbc316592b67
Joe Reid <joe@joereid.com>
2022-10-11 10:08:21 -0400

    use the same file (.q2servers.json) as other tools

--- q2info.go
+++ q2info.go
@@ -2,53 +2,55 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strings"
 	"time"
 )
 
-/**
- * Start here
- */
+//
+// Structure to hold our config file ~/.q2servers.json
+// This is a small subset of the data in that file,
+// just what we need to perform our duty
+//
+type ServerJSON struct {
+	Servers []struct {
+		Name string `JSON:"name"`
+		Addr string `JSON:"addr"`
+	} `JSON:"servers"`
+}
+
+const (
+	DefaultPort = ":27910"
+)
+
+var (
+	Config  ServerJSON
+	SrvFile *string // flag
+	Verbose *bool   // flag
+)
+
+//
+// init() is called before this
+//
 func main() {
-	// no args given, show usage
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <server_ip[:port]|alias> [keylookup]\n", os.Args[0])
-		fmt.Printf("  ex: %s 192.0.2.45:27911\n", os.Args[0])
-		fmt.Printf("  ex: %s frag.gr:27940\n", os.Args[0])
-		fmt.Printf("  ex: %s nj.packetflinger.com\n", os.Args[0])
-		fmt.Printf("  ex: %s tastyspleen.net:27916 players\n", os.Args[0])
-		fmt.Printf("  ex: %s dmsrv player_count\n", os.Args[0])
-		return
-	}
-
-	aliases := map[string]string{}
-
-	dirname, err := os.UserHomeDir()
-	if err == nil {
-		aliasfile := fmt.Sprintf("%s/.q2info", dirname)
-		aliases = LoadAliases(aliasfile)
-	}
-
-	server := os.Args[1]
-	alias := aliases[server]
-	if alias == "" {
-		if !strings.Contains(server, ":") {
-			server = server + ":27910"
-		}
-	} else {
-		server = alias
-	}
+	server := GetServer(flag.Arg(0))
 
 	// user included a specific value to get
 	lookup := ""
-	if len(os.Args) == 3 {
-		lookup = os.Args[2]
+	if len(flag.Args()) == 2 {
+		lookup = flag.Arg(1)
 	}
 
 	p := make([]byte, 1500)
+
+	if *Verbose {
+		log.Printf("Connecting to %s\n", server)
+	}
 
 	// only use IPv4
 	conn, err := net.Dial("udp4", server)
@@ -61,9 +63,12 @@ func main() {
 
 	cmd := []byte{0xff, 0xff, 0xff, 0xff}
 	cmd = append(cmd, "status"...)
-	fmt.Fprintln(conn, string(cmd))
 
+	starttime := time.Now()
+	fmt.Fprintln(conn, string(cmd))
 	_, err = bufio.NewReader(conn).Read(p)
+	duration := time.Since(starttime)
+
 	if err != nil {
 		fmt.Println("Read error:", err)
 		return
@@ -77,11 +82,35 @@ func main() {
 	} else {
 		PrintServerVars(info)
 	}
+
+	if *Verbose {
+		log.Printf("Results fetched in %s\n", duration.String())
+	}
 }
 
-/**
- * Load the backslash delimited infostring into a key value map
- */
+//
+// Either locate the server reference in the config file or assume
+// it was given manually at runtime and format it properly with
+// a port
+//
+func GetServer(alias string) string {
+	for _, s := range Config.Servers {
+		if strings.EqualFold(s.Name, alias) {
+			return s.Addr
+		}
+	}
+
+	// not found in config file, assuming arg is the address
+	if !strings.Contains(alias, ":") {
+		alias = alias + DefaultPort
+	}
+
+	return alias
+}
+
+//
+// Load the backslash delimited infostring into a key value map
+//
 func ParseServerinfo(s []string) map[string]string {
 	serverinfo := s[1][1:]
 	playerinfo := s[2 : len(s)-1]
@@ -109,54 +138,69 @@ func ParseServerinfo(s []string) map[string]string {
 	return info
 }
 
-/**
- * You can store a file of key-value pairs mapping names to server ip:ports
- */
-func LoadAliases(aliasfile string) map[string]string {
-	raw, err := os.ReadFile(aliasfile)
-	if err != nil {
-		return map[string]string{}
-	}
-
-	aliases := map[string]string{}
-	lines := strings.Split(string(raw), "\n")
-	for _, line := range lines {
-		trimmedline := strings.TrimSpace(line)
-		if trimmedline == "" {
-			continue
-		}
-
-		if strings.HasPrefix(trimmedline, "#") {
-			continue
-		}
-
-		if strings.HasPrefix(trimmedline, "//") {
-			continue
-		}
-
-		alias := strings.Fields(line)
-		aliases[alias[0]] = alias[1]
-	}
-
-	return aliases
-}
-
-/**
- * Just spit everything to stdout
- */
+//
+// Just spit everything to stdout
+//
 func PrintServerVars(info map[string]string) {
 	for k, v := range info {
 		fmt.Printf("%s: %s\n", k, v)
 	}
 }
 
-/**
- * Print out only the value for the given key
- */
+//
+// Print out only the value for the given key
+//
 func PrintSpecificVar(info map[string]string, lookup string) {
 	for k, v := range info {
-		if k == strings.ToLower(lookup) {
+		if strings.EqualFold(k, lookup) {
 			fmt.Printf("%s\n", v)
 		}
+	}
+}
+
+//
+// Called before main()
+//
+func init() {
+
+	// parse args
+	SrvFile = flag.String("c", "", "Specify a server data file")
+	Verbose = flag.Bool("v", false, "Show some more info")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Printf("Usage: %s <flags> <serveralias> [property]\n", os.Args[0])
+		fmt.Printf("  flags:\n")
+		flag.PrintDefaults()
+		fmt.Println("  [property] is optional and can be any one key value returned")
+		fmt.Println("    ex: players, hostname, dmflags")
+		os.Exit(0)
+	}
+
+	if *SrvFile == "" {
+		homedir, err := os.UserHomeDir()
+		sep := os.PathSeparator
+		if err != nil {
+			log.Fatal(err)
+		}
+		*SrvFile = fmt.Sprintf("%s%c.q2servers.json", homedir, sep)
+	}
+
+	if *Verbose {
+		log.Printf("Loading passwords/servers from %s\n", *SrvFile)
+	}
+
+	raw, err := os.ReadFile(*SrvFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(raw, &Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *Verbose {
+		log.Printf("  %d servers found\n", len(Config.Servers))
 	}
 }
